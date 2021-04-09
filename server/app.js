10fb1c6cab88d78ef954dcf2df32ffde3914ff4f
Vie <the_ruesi@zoho.com>
2021-04-09 12:45:15 +0100

    init commit

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+const PORT  = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, '..', 'build' )
+
+console.log(publicPath)
+
+
+app.use(express.static(publicPath))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log('Ole cloth ears is listening on port ', PORT)
+})
